cc264e61310e79e76da274142de19ae395e255fe
dashdavid13 <davidamedina619@gmail.com>
2021-04-17 22:03:53 -0400

    adding paths

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Bio from "./Bio"
+import Resume from "./resume"
+import Skills from "./Skills"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Switch>
+       <Route exact path= "/">
+        <Nav />
+       </Route>
+       <Route exact path= "/bio">
+        <Bio />
+       </Route>
+       <Route exact path= "/resume">
+        <Resume />
+       </Route>
+       <Route exact path= "/skills">
+        <Skills />
+       </Route>
+
+     </Switch>
     </div>
   );
 }
