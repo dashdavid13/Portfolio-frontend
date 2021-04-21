92b5161e30f2b946341393779695499e2dfddb0a
dashdavid13 <davidamedina619@gmail.com>
2021-04-20 22:59:06 -0400

    pdf file is done, going to add download button

--- src/Resume.js
+++ src/Resume.js
@@ -1,15 +1,28 @@
-import React  from 'react';
+import React, { useState } from 'react';
 import resume from './info/resume.pdf';
-import SinglePagePDFViewer from "./Single.js";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
- 
- 
-    return(
+  const [numPages, setNumPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
     <div>
-      <SinglePagePDFViewer pdf={resume} />
+      <Document
+        className="resume"
+        file={resume}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>Page {pageNumber} of {numPages}</p>
     </div>
-    )
+  );
 }
 
 export default Resume
