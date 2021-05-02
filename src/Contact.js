cceee3077cb28b47e0450f51d1c27175e2de8249
dashdavid13 <davidamedina619@gmail.com>
2021-05-02 17:28:56 -0400

    contacts are done

--- src/Contact.js
+++ src/Contact.js
@@ -5,8 +5,13 @@ function Contact() {
 
     return(
         <div className="contact">
-            <h1>contact</h1>
-            <text>Email</text>
+            <h1>Contact Me</h1>
+            <h4>Get in touch with me → davidamedina619@gmail.com</h4>
+            <br />
+            <h5>or find me on:</h5>
+            <a href="https://www.linkedin.com/in/david-medina-484039207/">Linkedin</a>
+            <a href="https://github.com/dashdavid13"> Github</a>
+            <a href="https://davidamedina619.medium.com/">Medium</a>
         </div>
     )
 }
